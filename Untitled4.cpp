0506feef24c7d586d02732bba1396350e43cf834
luizantoniobecker02-create <luizantoniobecker02@gmail.com>
2026-08-18 20:32:29 -0300

    Add files via upload

---
+++ Untitled4.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+ 
+ int main(){//Classe principal
+ 	int opcao;//Cria uma selecao de opcoes
+ 	
+ 	printf("===Menu Principal=== \n");//Apresenta uma informacao na tela
+ 	printf("1. Iniciar jogo \n");//Apresenta uma info na tela
+ 	printf("2. Carregar jogo \n");//Apresenta uma info na tela
+ 	printf("3. Configuracoes \n");//Apresenta uma info na tela
+ 	printf("4. Sair. \n");//Apresenta uma info na tela
+ 	scanf("%d", &opcao);//Captura uma variavel do tipo - &Opcao
+ 	
+ 	switch (opcao){//Cria uma selecao de opcoes de 1 ate 4
+ 		
+ 		case 1://Caso 1
+ 			printf("\n iniciando um novo jogo...\n");
+ 			break;//Para na case 1
+ 			
+ 		case 2://Case 2
+		 printf("\n Carregando um save interior... \n");
+		 break;//Para na case 2
+		 
+		case 3://Case 3
+			printf("\n Abrindo painel de configuracoes... \n");
+			break;//Para na case 3
+			
+		case 4://Case 4
+		    printf("\n Saindo do programa, ate mais... \n");
+		    break;//Para na case 4
+		    
+	    default://Ele nao encontra a opcao 1 ate 4, mostra um erro.
+		    printf("\n Opcao invalida, tente de 1 ate 4. \n");
+		    break;//Para no default
+	 }
+	 
+	 return 0;
+ }
