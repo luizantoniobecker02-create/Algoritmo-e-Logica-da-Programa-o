4e4dd06e0259c926a47261704335da75f2fb287e
Luiz Antonio Becker <luizantoniobecker02@gmail.com>
2026-08-24 22:01:51 -0300

    Add files via upload

---
+++ Untitled9.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	printf("---Alfabeto com WHILE---\n\n");
+	printf("Letra | Codigo ASCII\n");
+	printf("----------------------\n");
+	
+	//1.inicializacao da variavel do tipo char antes do while 
+	char letra = 'A';
+	
+	//2.condicao: o loop roda enquanto a letra for menor ou igual a 'Z'
+	while (letra <= 'Z') {
+		//exibe a letra e o seu codigo ASCII correspondente 
+		printf("%c | %d\n", letra, (int)letra);
+		
+		//3.incremento: passa para a proxima letra do alfabeto
+		letra++;
+	}
+	return 0;
+}
