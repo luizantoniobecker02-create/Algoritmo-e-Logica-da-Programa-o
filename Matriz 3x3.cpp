d7319bb45e3bd0612099dbcae091e768303c31af
Luiz Antonio Becker <luizantoniobecker02@gmail.com>
2026-09-01 19:50:08 -0300

    Matriz 3x3

--- Matriz 3x3.cpp
+++ Matriz 3x3.cpp
@@ -1,15 +1,15 @@
 #include<stdio.h>
 
-int main(){
-	int matriz[3][3]={
+int main(){//Decalarando uma variavel
+	int matriz[3][3]={//tranalha com numeros inteiros
 	{10,20,30},
 	{40,50,60},
 	{70,80,90},
 	};
 	
-	printf("Exibindo a matriz completa:\n");
+	printf("Exibindo a matriz completa:\n");//exibe a  matriz completa
 	
-	for (int i=0;i<3;i++){
+	for (int i=0;i<3;i++){//indica que o FOR ira trabalhar ate encontrar o numero 3
 		for(int j=0; j<3;j++){
 			printf("%d", matriz[i][j]);
 			
@@ -17,6 +17,6 @@ int main(){
 		}
 		printf("\n");
 	}
-	printf("\nElemento na posicao [1][2]: %d\n", matriz[1][2]);
+	printf("\nElemento na posicao [1][2]: %d\n", matriz[1][2]);//mostra a posicao das linhas e colunas
 	return 0;
 }
